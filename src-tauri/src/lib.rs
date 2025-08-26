f9ec0b04e06fb57ae9a2a4c4bc05db1114896cd2
tbjoseph <timothy.b.joseph@outlook.com>
2025-08-26 18:55:30 -0500

    removing dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,7 +13,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_persisted_scope::init())
-        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         // Optional: while developing, hard-allow your repo `saves` dir:
